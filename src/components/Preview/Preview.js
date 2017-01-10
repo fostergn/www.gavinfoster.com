d4e50f993c5d6dc2f22325736be26757e7b5604a
Gavin Foster <fostergn@vcu.edu>
2017-01-10 01:33:32 -0500

    adding preview image

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -8,11 +8,20 @@ const Preview = ({ project, hovering }) =>  {
     'project-preview__title--hidden': !hovering
   });
 
+  const previewImageClasses = classNames({
+    'project-preview__image': true,
+    'project-preview__image--hidden': !hovering
+  });
+
   return (
     <div className="project-preview__container">
       <div className={previewTitleClasses}>{project.name}</div>
-      <section className="project-preview__image-container" style={{backgroundImage: `url('${project.cover}')`}}>
-      </section>
+      {/*<section className="project-preview__image-container" style={{backgroundImage: `url('${project.cover}')`}}>*/}
+      <section className="project-preview__image-container">
+        <div className="project-preview__image-container-child">
+          <img className={previewImageClasses} src={project.cover} />
+        </div>
+    </section>
     </div>
   );
 }

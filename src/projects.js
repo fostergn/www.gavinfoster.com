8d46384bfb63a3736c1940db574cb89aa1494fff
Gavin Foster <fostergn@vcu.edu>
2017-01-11 01:17:48 -0500

    changing chat styles

--- src/projects.js
+++ src/projects.js
@@ -13,7 +13,7 @@ const projects = [
   {
     name: 'portfolio-client',
     title: 'Portfolio Client',
-    description: 'here it goes are you ready',
+    description: `The project is a quiz of random holiday questions that generates a lottery scratcher based on answers. The site uses <span class="highlight--blue">React</span> and <span class="highlight--purple">Redux</span> to handle <span class="underline--blue">state</span> and <span class="highlight--orange">GSAP</span> to create <span class="underline--orange">animations</span> 📹. Users' answers are pushed to a Firebase realtime database 🔥`,
     imgs: [portrait, lotteryImage],
     cover: portrait,
     backgroundColor: 'aliceblue',
@@ -21,7 +21,7 @@ const projects = [
   {
     name: 'portfolio-admin',
     title: 'Portfolio Admin',
-    description: 'here it goes are you ready',
+    description: `The project is a quiz of random holiday questions that generates a lottery scratcher based on answers. The site uses <span class="highlight--blue">React</span> and <span class="highlight--purple">Redux</span> to handle <span class="underline--blue">state</span> and <span class="highlight--orange">GSAP</span> to create <span class="underline--orange">animations</span> 📹. Users' answers are pushed to a Firebase realtime database 🔥`,
     imgs: [lotteryImage, portrait],
     cover: portrait,
     backgroundColor: 'beige',

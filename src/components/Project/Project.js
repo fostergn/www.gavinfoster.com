93f5817d51e9a8a4185fddd57c42102986b5fa51
Gavin Foster <fostergn@vcu.edu>
2017-01-28 15:54:31 -0500

    adding images and infinite scroll

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import {browserHistory} from 'react-router';
 
 class Project extends Component {
@@ -12,10 +13,112 @@ class Project extends Component {
     }
   }
 
+  componentDidMount(){
+  //   this.isMobile = false;
+  //   const breakpoint = 700;
+  //   let windowWidth = window.innerWidth;
+  //   this.isMobile = windowWidth < breakpoint;
+  //   console.log('window width: ', windowWidth);
+  //   var doc = window.document,
+  //   context = doc.getElementsByClassName('project-images__container')[0],
+  //   clones = context.getElementsByClassName('project-images__item--clone'),
+  //   disableScroll = false,
+  //   scrollHeight = 0,
+  //   scrollPos = 0,
+  //   clonesHeight = 0,
+  //   i = 0;
+
+  // function getScrollPos() {
+  //   return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
+  // }
+
+  // function setScrollPos(pos) {
+  //   context.scrollTop = pos;
+  // }
+
+  // function getClonesHeight() {
+  //   clonesHeight = 0;
+  //   i = 0;
+
+  //   for (i; i < clones.length; i += 1) {
+  //     clonesHeight = clonesHeight + clones[i].offsetHeight;
+  //   }
+
+  //   return clonesHeight;
+  // }
+
+  // function reCalc() {
+  //   scrollPos = getScrollPos();
+  //   scrollHeight = context.scrollHeight;
+  //   clonesHeight = getClonesHeight();
+
+  //   if (scrollPos <= 0) {
+  //     setScrollPos(1); // Scroll 1 pixel to allow upwards scrolling
+  //   }
+  // }
+
+  // // Calculate variables
+  // window.requestAnimationFrame(reCalc);
+
+  // function scrollUpdate() {
+  //   if (!disableScroll) {
+  //     scrollPos = getScrollPos();
+
+  //     if (clonesHeight + scrollPos >= scrollHeight) {
+  //       // Scroll to the top when you’ve reached the bottom
+  //       setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
+  //       disableScroll = true;
+  //     } else if (scrollPos <= 0) {
+  //       // Scroll to the bottom when you reach the top
+  //       setScrollPos(scrollHeight - clonesHeight);
+  //       disableScroll = true;
+  //       }
+  //     }
+
+  //     if (disableScroll) {
+  //       // Disable scroll-jumping for a short time to avoid flickering
+  //       window.setTimeout(function () {
+  //         disableScroll = false;
+  //       }, 40);
+  //     }
+  //   }
+
+  //   context.addEventListener('scroll', function () {
+  //     window.requestAnimationFrame(scrollUpdate);
+  //   }, false);
+
+  //   window.addEventListener('resize', function () {
+  //     window.requestAnimationFrame(reCalc);
+  //   }, false);
+
+
+    // Just for the demo: Center the middle block on page load
+    // window.onload = function () {
+    //   setScrollPos(Math.round(clones[0].getBoundingClientRect().top + getScrollPos() - (window.innerHeight - clones[0].offsetHeight) / 2));
+    // };
+  }
+
   render(){
-    const { title, description, imgs, backgroundColor } = this.props.project;
-    const imgsArray = Array.isArray(imgs) ? imgs : [];
-    const imgsList = imgsArray.map((img, index) => <div key={index} className="project-images__item"><img className="project-images__image" src={img} /></div>)
+    console.log('project props: ', this.props);
+    const { location } = this.props;
+    const { title, description, imgs, backgroundColor, backgroundColorBorder } = this.props.project;
+
+    const shadowStyle= { boxShadow: `0 8px 8px -8px ${backgroundColorBorder} inset, 0 -8px 8px -8px ${backgroundColorBorder} inset`};
+
+    // assuming atleast two images, copy for infinite loop
+    // cloning two images for looping scroll
+    const imgsArray = Array.isArray(imgs) ? [...imgs, imgs[0], imgs[1]] : [];
+    let imgsList = imgsArray.map((img, index) => {
+      let imgClass = classNames({
+        'project-images__item': true,
+        'project-images__item--clone': imgsArray.length - index <= 2,
+      });
+      return (
+        <div key={index} className={imgClass}>
+          <img className="project-images__image" srcSet={img} />
+         </div>
+      )
+    });
 
     return (
       <section className="project-single__container" style={{backgroundColor: backgroundColor}}>
@@ -26,7 +129,7 @@ class Project extends Component {
           </div>
         </div>
         <div className="project-single__image-container">
-          <div className="project-images__container">
+          <div className="project-images__container" style={shadowStyle}>
             {imgsList}
           </div>
         </div>
--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,14 @@
-import lotteryImage from './images/holiday-games.png';
-import lotteryImage2 from './images/holiday-games2.png';
-import lotteryImage3 from './images/holiday-games3.png';
+import chmCreateBrowser from './images/chm-create-browser.png';
+import chmSentBrowser from './images/chm-sent-browser.png';
+import chmReceiptsBrowser from './images/chm-receipts-browser.png';
+import chmDashboardMobile from './images/chm-dashboard-mobile.png';
+
+import holidayGamesBrowser from './images/holiday-games-browser.png';
+import holidayGiftBrowser from './images/holiday-gift-browser.png';
+import findingGiftBrowser from './images/finding-gift-browser.png';
+import holidayGamesMobile from './images/holiday-games-mobile.png';
+
+import chmCoverImage from './images/business-stock.jpg';
 import holidayCoverImage from './images/holiday-cover.png';
 import portrait from './images/portrait.jpg';
 
@@ -9,25 +17,19 @@ const projects = [
     name: 'lottery',
     title: 'VA Lottery Holiday Games',
     description: `The project is a quiz of random holiday questions that generates a lottery scratcher based on answers. The site uses <span class="highlight--blue">React</span> and <span class="highlight--purple">Redux</span> to handle <span class="underline--blue">state</span> and <span class="highlight--orange">GSAP</span> to create <span class="underline--orange">animations</span> 📹. Users' answers are pushed to a Firebase realtime database 🔥`,
-    imgs: [lotteryImage, lotteryImage2, lotteryImage3],
+    imgs: [holidayGamesBrowser, holidayGiftBrowser, findingGiftBrowser, holidayGamesMobile],
     cover: holidayCoverImage,
-    backgroundColor: 'azure',
+    backgroundColor: 'rgba(200, 200, 255, 1)',
+    backgroundColorBorder: 'rgba(200, 200, 255,.6)',
   },
   {
-    name: 'portfolio-client',
-    title: 'Portfolio Client',
-    description: `The project is a quiz of random holiday questions that generates a lottery scratcher based on answers. The site uses <span class="highlight--blue">React</span> and <span class="highlight--purple">Redux</span> to handle <span class="underline--blue">state</span> and <span class="highlight--orange">GSAP</span> to create <span class="underline--orange">animations</span> 📹. Users' answers are pushed to a Firebase realtime database 🔥`,
-    imgs: [portrait, lotteryImage],
-    cover: portrait,
-    backgroundColor: 'aliceblue',
-  },
-  {
-    name: 'portfolio-admin',
-    title: 'Portfolio Admin',
-    description: `The project is a quiz of random holiday questions that generates a lottery scratcher based on answers. The site uses <span class="highlight--blue">React</span> and <span class="highlight--purple">Redux</span> to handle <span class="underline--blue">state</span> and <span class="highlight--orange">GSAP</span> to create <span class="underline--orange">animations</span> 📹. Users' answers are pushed to a Firebase realtime database 🔥`,
-    imgs: [lotteryImage, portrait],
-    cover: portrait,
-    backgroundColor: 'beige',
+    name: 'internal messaging tool',
+    title: 'Internal Messaging Tool',
+    description: `The messaging tool allows managers to message their team and coworkers. Connecting <span class="highlight--orange">Drupal</span>, an ERM, and <span class="highlight--blue">Twilio</span> allows for sending and tracking <span class="underline--blue">MMS</span> that helps foster a rapidly growing company's culture.`,
+    imgs: [chmCreateBrowser, chmSentBrowser, chmReceiptsBrowser, chmDashboardMobile],
+    cover: chmCoverImage,
+    backgroundColor: 'rgba(200, 200, 255, 1)',
+    backgroundColorBorder: 'rgba(200, 200, 255,.6)',
   },
 ];
 
